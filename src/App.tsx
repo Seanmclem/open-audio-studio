2b3800d3e279cd2633c5b01054a99fa024cbb0f0
seanmclem <seanmclements@gmail.com>
2020-09-26 01:22:08 -0400

    add packages, basic styled component

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import styled from 'styled-components'
+
+const Header = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
+      <Header>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,7 +31,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </Header>
     </div>
   );
 }
